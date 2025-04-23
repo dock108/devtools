21e990a4a8aef84c40911307d67d13e5f07ff286
highlyprofitable108 <mikefusc@me.com>
2025-04-22 21:23:12 -0400

    chore(middleware): change runtime configuration to 'experimental-edge' for improved performance

--- middleware.ts
+++ middleware.ts
@@ -32,5 +32,5 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
-  runtime: 'edge',
+  runtime: 'experimental-edge',
 };
